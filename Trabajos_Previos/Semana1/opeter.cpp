334912a6fbf29d781448a14c233be75cf8093de9
Aurelio Cordoba V <acvalerio25@gmail.com>
2024-01-04 03:19:11 -0600

    Aca se sube la prueba del operador ternario
    Signed-off-by: Aurelio Cordoba V <acvalerio25@gmail.com>

---
+++ Trabajos_Previos/Semana1/opeter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int var1 = 0;
+    int var2;
+
+    var2 = (var1 ==0) ? 15 : 27;
+    cout << "var2 es : \n" << var2 << "";
+
+    return 0;
+}
