a8540ccd34d9d94b0ed111982d264270a72b2415
Aurelio Cordoba V <acvalerio25@gmail.com>
2024-01-04 03:02:31 -0600

    Aca se sube la prueba de las variables
    Signed-off-by: Aurelio Cordoba V <acvalerio25@gmail.com>

---
+++ Trabajos_Previos/Semana1/variable1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // variable local:
+    int a, b;
+    int c;
+
+    //inicializacion 
+    a = 10;
+    b = 20;
+    c = a + b;
+    cout << c;
+    return 0;
+}
