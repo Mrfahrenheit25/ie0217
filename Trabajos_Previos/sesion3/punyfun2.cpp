22132c2b0ccd83b62698b4584b8cda61bceea32f
Aurelio Cordoba V <acvalerio25@gmail.com>
2024-01-04 22:03:23 -0600

    Aca se sube los codigos de los punteros y funciones y de la suma
    Signed-off-by: Aurelio Cordoba V <acvalerio25@gmail.com>

---
+++ Trabajos_Previos/sesion3/punyfun2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* n1, int* n2){
+    int temp;
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+int main(){ 
+
+    int a = 1, b = 2;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    // llamar a la funcion que cambia
+    swap(&a, &b);
+
+    cout<<"Luego de cambiar" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    
+    return 0;
+}
